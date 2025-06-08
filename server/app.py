efb8bbec4da388129f012800196d71f09fe16714
Dave Banwell <dave@davebanwell.com>
2025-06-08 16:46:05 -0400

    passes all tests

--- server/app.py
+++ server/app.py
@@ -15,13 +15,41 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 
+# views
+
 @app.route('/')
 def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
 
+@app.route('/earthquakes/<int:id>')
+def quake_by_id(id):
+    quake = Earthquake.query.filter_by(id=id).first()
+    
+    if quake:
+        response_body = quake.to_dict()
+        response_status = 200
+    else:
+        response_body = {"message": f"Earthquake {id} not found."}
+        response_status = 404
+        
+    return make_response(response_body, response_status)
+
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def quakes_by_magnitude(magnitude):
+    quakes = [
+        quake.to_dict() for quake in Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    ]
+    
+    response_body = {
+        "count": len(quakes),
+        "quakes": quakes
+    }
+    
+    return make_response(response_body, 200)
+    
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
